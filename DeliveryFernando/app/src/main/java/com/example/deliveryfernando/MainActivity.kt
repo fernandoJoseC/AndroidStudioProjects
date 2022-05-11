a46962ffae2ab456b0637a24cc65ee16bbc1d414
fernandoJoseC <fernandocalderon05@gmail.com>
2022-05-10 19:21:52 -0500

    cambios visuales y mostrar user y password

--- DeliveryFernando/app/src/main/java/com/example/deliveryfernando/MainActivity.kt
+++ DeliveryFernando/app/src/main/java/com/example/deliveryfernando/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.deliveryfernando
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.deliveryfernando.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var views: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        views = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(views.root)
+        initializacionListeners()
+
+    }
+
+    private fun initializacionListeners(){
+        views.button.setOnClickListener {
+            //var textoRecogido = views.textInput.text
+            Toast.makeText(this, ("el usuario es "+ views.editTextTextPersonName.text + " la contrasena es "+views.editTextTextPassword.text), Toast.LENGTH_SHORT).show()
+
+        }
     }
 }
