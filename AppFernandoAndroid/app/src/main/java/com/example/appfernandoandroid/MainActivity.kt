40e0512bbbf6ede6c3f6e23e5cc64e4573d9b971
fernandoJoseC <fernandocalderon05@gmail.com>
2022-05-10 19:49:32 -0500

    subo el proyecto app fernando android

---
+++ AppFernandoAndroid/app/src/main/java/com/example/appfernandoandroid/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.appfernandoandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.appfernandoandroid.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var views: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        views = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(views.root)
+        initializacionListeners()
+
+    }
+
+    private fun initializacionListeners(){
+        views.button.setOnClickListener {
+            //var textoRecogido = views.textInput.text
+            Toast.makeText(this, ("el usuario es "+ views.editTextTextPersonName.text + " la contrasena es "+views.editTextTextPassword.text), Toast.LENGTH_SHORT).show()
+
+        }
+    }
+}
