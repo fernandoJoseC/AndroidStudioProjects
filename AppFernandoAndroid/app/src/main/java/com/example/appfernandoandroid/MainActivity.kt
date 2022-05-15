3617c652fcf732eb86f93af25a44224589815ee6
fernandoJoseC <fernandocalderon05@gmail.com>
2022-05-15 13:25:47 -0500

    cambios interface y main

--- AppFernandoAndroid/app/src/main/java/com/example/appfernandoandroid/MainActivity.kt
+++ AppFernandoAndroid/app/src/main/java/com/example/appfernandoandroid/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.appfernandoandroid
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.example.appfernandoandroid.databinding.ActivityMainBinding
+import com.example.appfernandoandroid.interface_user.interface_user
 
 
 class MainActivity : AppCompatActivity() {
@@ -18,11 +20,29 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    private fun initializacionListeners(){
+    private fun initializacionListeners() {
+
         views.button.setOnClickListener {
             //var textoRecogido = views.textInput.text
-            Toast.makeText(this, ("el usuario es "+ views.editTextUserName2.text + " la contrasena es "+views.editTextPassword2.text), Toast.LENGTH_SHORT).show()
+            //Toast.makeText(this, ("el usuario es "+ views.editTextUserName2.text + " la contrasena es "+views.editTextPassword2.text), Toast.LENGTH_SHORT).show()
+            val userText = views.editTextUserName2.text.toString()
+            val passwordText = views.editTextPassword2.text.toString()
 
+            userText?.let {
+                passwordText?.let { itp ->
+                    if (it.equals("fernando") && itp.equals("5311")) {
+                        goToPrincipal(userText)
+                    }
+                }
+            }
         }
     }
+
+    private fun goToPrincipal(userText:String){
+        var intent: Intent = Intent(this, interface_user::class.java)
+        intent.putExtra("name_user", userText)
+        startActivity(intent)
+    }
+
+
 }
--- AppFernandoAndroid/app/src/main/java/com/example/appfernandoandroid/interface_user/interface_user.kt
+++ AppFernandoAndroid/app/src/main/java/com/example/appfernandoandroid/interface_user/interface_user.kt
@@ -1,12 +1,28 @@
 package com.example.appfernandoandroid.interface_user
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.appfernandoandroid.R
+import com.example.appfernandoandroid.databinding.ActivityInterfaceUserBinding
+
 
 class interface_user : AppCompatActivity() {
+
+    private lateinit var views: ActivityInterfaceUserBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_interface_user)
+        views = ActivityInterfaceUserBinding.inflate(layoutInflater)
+        setContentView(views.root)
+        showName(intent)
     }
+
+    private fun showName(intent: Intent) {
+
+        val user_name = intent.getStringExtra("name_user")
+        views.showName.setText(user_name)
+
+    }
+
+
 }
