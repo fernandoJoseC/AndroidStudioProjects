f5cfbd921008497cea8656bd58c0e6336b4c0439
fernandoJoseC <fernandocalderon05@gmail.com>
2022-05-17 23:06:07 -0500

    rename de variables,docs,ids

---
+++ AppFernando/app/src/main/java/com/example/appfernando/interface_user/Principal_main.kt
@@ -0,0 +1,12 @@
+package com.example.appfernando.interface_user
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.appfernando.R
+
+class Principal_main : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_principal_main)
+    }
+}
